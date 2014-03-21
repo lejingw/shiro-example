f7212d3a389a93ad4028d7c7a05e125b931f3d9b
lejing wang <lejingw@163.com>
2014-03-21 09:57:09 +0800

    1

--- myshiro/src/main/java/com/github/lejingw/myshiro/dao/UserDaoImpl.java
+++ myshiro/src/main/java/com/github/lejingw/myshiro/dao/UserDaoImpl.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
